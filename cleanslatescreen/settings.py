3443aa369c35fccb17378a722407780eb4548fc5
Nate Vogel <nathan.c.vogel@gmail.com>
2019-12-04 13:29:11 -0500

    added gunicorn and fixed a couple settings issues, getting a demo deployent set up.

--- cleanslatescreen/settings.py
+++ cleanslatescreen/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%iek$82#pjyynjfg+*psie^@4zn8f)=6=2nae%nphp&*mst-c%'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get("DEBUG") == "TRUE" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
